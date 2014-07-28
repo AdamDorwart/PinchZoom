9f74528b221f213a10c963714fdc6705a241d343
Adam Dorwart <adamjdorwart@gmail.com>
2014-07-28 00:35:50 -0700

    Changed touch-action, probably will need preventDefaults

--- pinchZoom.js
+++ pinchZoom.js
@@ -200,7 +200,7 @@ function resize() {
 
 function setup( ) {
 
-    hammer = new Hammer.Manager(container, {touchAction: "pan-y"});
+    hammer = new Hammer.Manager(container, {touchAction: "auto"});
 
     hammer.add(new Hammer.Pan({ threshold: 0, pointers: 0 }));
     hammer.add(new Hammer.Pinch({ threshold: 0 })).recognizeWith(hammer.get('pan'));
